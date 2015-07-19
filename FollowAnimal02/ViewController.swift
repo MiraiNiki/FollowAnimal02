d8b3a11740a0b07fd54222dcae152b68ab6a203e
Niki Mirai <s1411400@coins.tsukuba.ac.jp>
2015-07-19 15:55:24 +0900

    initial commit

--- FollowAnimal02/ViewController.swift
+++ FollowAnimal02/ViewController.swift
@@ -10,10 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var aka01: UIButton!
+    @IBOutlet weak var aka02: UIButton!
+    @IBOutlet weak var ore01: UIButton!
+    let akakin: UIImage = UIImage(named:"akakin.png")!
+    let orekin: UIImage = UIImage(named:"orekin.png")!
+    
+    var haikei_x : CGFloat = 0
+    var haikei_y : CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        haikei_x = self.view.bounds.height
+        haikei_y = self.view.bounds.width
+        
+        NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "move01:", userInfo: nil, repeats: true)
+        NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: "move02:", userInfo: nil, repeats: true)
+        NSTimer.scheduledTimerWithTimeInterval(8.0, target: self, selector: "move03:", userInfo: nil, repeats: true)
     }
+    func move01(aka01: UIButton){
+        let x = arc4random_uniform(UInt32(self.haikei_x))
+        let y = arc4random_uniform(UInt32(self.haikei_y))
+        UIView.animateWithDuration(1.0, animations: {
+            self.aka01.center = CGPointMake(CGFloat(x), CGFloat(y))
+        })
+    }
+    func move02(aka02: UIButton){
+        let x = arc4random_uniform(UInt32(self.haikei_x))
+        let y = arc4random_uniform(UInt32(self.haikei_y))
+        UIView.animateWithDuration(1.0, animations: {
+            self.aka02.center = CGPointMake(CGFloat(x), CGFloat(y))
+        })
+    }
+
+    func move03(ore01: UIButton){
+        let x = arc4random_uniform(UInt32(self.haikei_x))
+        let y = arc4random_uniform(UInt32(self.haikei_y))
+        UIView.animateWithDuration(1.0, animations: {
+            self.ore01.center = CGPointMake(CGFloat(x), CGFloat(y))
+        })
+    }
+
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
